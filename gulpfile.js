2b1cd5a76ac6dc480766f120a271de9fa95f8dc3
jcapiznon <jcapiznon@reekoh.com>
2017-02-27 12:01:32 +0800

    for v3

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,50 @@
-'use strict';
+'use strict'
 
-var gulp     = require('gulp'),
-	mocha    = require('gulp-mocha'),
-	plumber  = require('gulp-plumber'),
-	jshint   = require('gulp-jshint'),
-	jsonlint = require('gulp-json-lint');
+let gulp = require('gulp')
+let mocha = require('gulp-mocha')
+let plumber = require('gulp-plumber')
+let jshint = require('gulp-jshint')
+let jsonlint = require('gulp-json-lint')
 
-var paths = {
-	js: ['*.js', '*/*.js', '*/**/*.js', '!node_modules/**'],
-	json: ['*.json', '*/*.json', '*/**/*.json', '!node_modules/**'],
-	tests: ['./test/*.js']
-};
+let paths = {
+  js: ['*.js', '*/*.js', '*/**/*.js', '!node_modules/**'],
+  json: ['*.json', '*/*.json', '*/**/*.json', '!node_modules/**'],
+  tests: ['./test/*.js']
+}
 
-gulp.task('jslint', function () {
-	return gulp.src(paths.js)
-		.pipe(plumber())
-		.pipe(jshint())
-		.pipe(jshint.reporter('default'))
-		.pipe(jshint.reporter('fail'));
-});
+gulp.task('jslint', () => {
+  return gulp.src(paths.js)
+    .pipe(plumber())
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(jshint.reporter('fail'))
+})
 
-gulp.task('jsonlint', function () {
-	return gulp.src(paths.json)
-		.pipe(plumber())
-		.pipe(jsonlint({
-			comments: true
-		}))
-		.pipe(jsonlint.report());
-});
+gulp.task('jsonlint', () => {
+  return gulp.src(paths.json)
+    .pipe(plumber())
+    .pipe(jsonlint({
+      comments: true
+    }))
+    .pipe(jsonlint.report())
+})
 
-gulp.task('run-tests', function () {
-	return gulp
-		.src(paths.tests, {
-			read: false
-		})
-		.pipe(mocha({
-			reporter: 'list'
-		}))
-		.once('error', function (error) {
-			console.error(error);
-			process.exit(1);
-		})
-		.once('end', function () {
-			process.exit();
-		});
-});
+gulp.task('run-tests', () => {
+  return gulp
+    .src(paths.tests, {
+      read: false
+    })
+    .pipe(mocha({
+      reporter: 'list'
+    }))
+    .once('error', (error) => {
+      console.error(error)
+      process.exit(1)
+    })
+    .once('end', () => {
+      process.exit()
+    })
+})
 
-gulp.task('lint', ['jslint', 'jsonlint']);
-gulp.task('test', ['lint', 'run-tests']);
+gulp.task('lint', ['jslint', 'jsonlint'])
+gulp.task('test', ['lint', 'run-tests'])
